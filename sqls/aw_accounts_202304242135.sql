c1c22dcb7bebad3b38ae98e98c0d32502e84d057
houqingxiang_1 <houqingxiang_1@163.com>
2023-04-24 21:45:43 +0800

    sql

---
+++ sqls/aw_accounts_202304242135.sql
@@ -0,0 +1,5 @@
+INSERT INTO health.aw_accounts (name,username,password,avatar,sex,department,location,introduction,role_id,created_at,updated_at) VALUES
+	 ('test1','test1','$2b$10$RtdxK2va561pZRtA1eT1q.qvngJRTfOmi4Taq2XJMfWpNKg7lpHKi','http://localhost:3000/upload/20210612/16234847490027294.png',NULL,NULL,NULL,'个人介绍',3,'2023-03-26 00:48:20','2023-03-26 00:48:20'),
+	 ('test2','test2','$2b$10$kzpfTuaWIWK6XgRGiqIrKOCCPTbtIQwLqm/WSOBJ770d6a3PVAt6S','http://localhost:3000/upload/20210612/16234847490027294.png',NULL,NULL,NULL,'个人介绍',3,'2023-03-26 09:04:27','2023-03-26 09:04:27'),
+	 ('test3','test3','$2b$10$jzTFfsWVWqBDmnTTYB136OX0JN1SmB6fJoioZ6HDPP.gNFnpNDg.W','http://localhost:3000/upload/20210612/16234847490027294.png',NULL,NULL,NULL,'个人介绍',3,'2023-03-26 09:08:11','2023-03-26 09:08:11'),
+	 ('user1','user1','$2b$10$kMpQ7m0q3uAYhPBozsouUu2YKMQlpU1A9aLK3Wqo/VMx8EibxPUsC','http://localhost:3000/upload/20210612/16234847490027294.png',NULL,NULL,NULL,'个人介绍',3,'2023-04-02 21:46:21','2023-04-02 21:46:21');
---
+++ sqls/aw_xuewei_categories_202304242139.sql
@@ -0,0 +1,6 @@
+INSERT INTO health.aw_xuewei_categories (title,description,uni_key,create_time,update_time) VALUES
+	 ('手太阴肺经','','shoutaiyinfeijing','2023-04-08 11:09:56','2023-04-08 11:09:56'),
+	 ('手阳明大肠经','','shouyangmingdachangjing','2023-04-08 11:10:34','2023-04-08 11:10:34'),
+	 ('足阳明胃经','','zuyangmingweijing','2023-04-08 11:10:53','2023-04-08 11:10:53'),
+	 ('足太阴脾经','','zutaiyinpijing','2023-04-08 11:11:13','2023-04-08 11:11:13'),
+	 ('手少阴心经','','shoushaoyinxinjing','2023-04-08 11:11:33','2023-04-08 12:07:50');
